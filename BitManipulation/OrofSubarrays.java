99366a493af255eb696ed1ae55a0830f8238dd8c
Chetan <chetankolakanti27@gmail.com>
2025-03-10 09:50:28 +0530

    added the solution for Or of Subarr

--- BitManipulation/OrofSubarrays.java
+++ BitManipulation/OrofSubarrays.java
@@ -44,3 +44,22 @@
 // Explanation
 // Only one sum sub-arrays i.e. 0.
 
+import java.util.*;
+public class BitwiseOr{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int or = 0;
+        int sum=0;
+        for(int i=0;i<n;i++){
+            or = or | arr[i] | sum;
+            sum += arr[i];
+        }
+        or = or | sum;
+        System.out.println(or);
+    }
+}
